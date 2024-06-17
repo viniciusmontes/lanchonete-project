15855b79af23192e0086f0050269207d08a57c85
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-06-17 18:35:05 -0300

    Changes to card container image size

--- frontweb/src/components/Card/index.tsx
+++ frontweb/src/components/Card/index.tsx
@@ -13,7 +13,9 @@ export default function Card({ product }: Props) {
     <Link to={`/product-details/${product.id}`}>
       <div className="card-container">
         <div key={product.id} className="card-content">
+          <div className="card-content-image-container">
           <img src={product.imgUrl} alt="" />
+          </div>
           <h1 className="card-content-title">{limitText(product.name, 25)}</h1>
           <p className="card-content-price">R$ {formatPrice(product.price)}</p>
           <span>{limitText(product.description, 100)}</span>
